b12a22fc50f6e21e730f4e49f33e9661c916f825
Kelly Nyachiro Chanai <kellynyachiro@gmail.com>
2025-11-16 15:05:43 +0300

    latest

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -315,7 +315,7 @@ class EmailService {
     }
   }
 
-  // function to Generate OTP the Code
+  // function Generate OTP the Code
   generateOTP() {
     return Math.floor(100000 + Math.random() * 900000).toString();
   }
